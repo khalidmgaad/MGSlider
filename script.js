2a84c05b56f2f0485616741c38237b3142ded9b4
admin <admin@admins-MacBook-Pro.local>
2020-02-12 23:36:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var sliderItems = document.getElementsByClassName("slider-item");
+console.log("Items "+sliderItems.length);
+var currentIndex = 0;
+function showNext() {
+  currentIndex = currentIndex<sliderItems.length-1 ? currentIndex+1 : 0;
+  for(i=0;i<sliderItems.length;i++){
+    sliderItems[i].classList.remove("active");
+  }
+  sliderItems[currentIndex].classList.add("active");
+}
+
+function showPrev() {
+  currentIndex = currentIndex>0 ? currentIndex-1 : sliderItems.length-1;
+  for(i=0;i<sliderItems.length;i++){
+    sliderItems[i].classList.remove("active");
+  }
+  sliderItems[currentIndex].classList.add("active");
+}
+
+
+// ADDED WITH COMMIT COMMAND
